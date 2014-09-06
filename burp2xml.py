83650d648bd95684a1a3fff33267f3ba5f02a945
Lukas Kuzmiak <lukas.kuzmiak@insighti.org>
2014-09-06 07:19:53 +0200

    buffered reading
    read file by chunks, takes ~20% more time, uses ~5% of previous memory
    usage

--- burp2xml.py
+++ burp2xml.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-#Developed by Paul Haas, <phaas AT redspin DOT com> under Redspin. Inc.
-#Licensed under the GNU Public License version 3.0 (2008-2009)
+# Developed by Paul Haas, <phaas AT redspin DOT com> under Redspin. Inc.
+# Licensed under the GNU Public License version 3.0 (2008-2009)
+# Forked and further improved by Lukas Kuzmiak, <lukas.kuzmiak AT insighti DOT org> under insighti a.s.
+
 '''Process Burp Suite Professional's output into a well-formed XML document.
 
 Burp Suite Pro's session file zipped into a combination of XML-like tags 
@@ -13,7 +15,10 @@ loading the results in Burp.
 This tool takes a zipped Burp file and outputs a XML document based upon the
 provided arguments which allows regular XPATH queries and XSL transformations.
 '''
+
 import datetime, string, re, struct, zipfile, sys
+
+CHUNK_SIZE = 10240 # use negative number to read the whole file at once
 
 TAG = re.compile('</?(\w*)>',re.M) # Match a XML tag
 nvprint = string.printable.replace('\x0b','').replace('\x0c','') # Printables
@@ -26,104 +31,130 @@ def milliseconds_to_date(milliseconds):
     So we convert the milliseconds to seconds first as a POSIX timestamp which
     can be used to get a valid date, and then use the parsed values from that
     object along with converting mili -> micro seconds in a new date object.'''
+
     try:
         d = datetime.datetime.fromtimestamp(milliseconds/1000)
         date = datetime.datetime(d.year,d.month,d.day,d.hour,d.minute,d.second,
             (milliseconds%1000)*1000)        
-    except ValueError, e: # Bad date, just return the milliseconds
+    except ValueError: # Bad date, just return the milliseconds
         date = str(milliseconds)
     return date    
 
-def burp_binary_field(field,i,non_printable):
-    '''Strip Burp Suite's binary format characters types from our data.    
-    The first character after the leading tag describes the type of the data.
-        If non_printable is true, keep nonprintable characters
-        '''
-    if len(field) <= i:
-        return None,-1
-    elif field[i] == '\x00': # 4 byte integer value
-        return str(struct.unpack('>I',field[i+1:i+5])[0]),5        
-    elif field[i] == '\x01': # Two possible unsigned long long types
-        if field[i+1] == '\x00': # (64bit) 8 Byte Java Date
-            ms = struct.unpack('>Q',field[i+1:i+9])[0]        
-            date = milliseconds_to_date(ms)
-            if type(date) == str:
-                value = date
-            else:
-                value =    date.ctime() # Use the ctime string format for date
-        else: # Serial Number only used ocasionally in Burp
-            value = str(struct.unpack('>Q',field[i+1:i+9])[0])
-        return value,9
-    elif field[i] == '\x02': # Boolean Object True/False
-        return str(struct.unpack('?',field[i+1:i+2])[0]),2        
-    elif field[i] == '\x03' or field[i] == '\x04': # 4 byte length + string        
-        length = struct.unpack('>I',field[i+1:i+5])[0]
-        #print "Saw string of length",length,"at",i+5,i+5+length
-        value = field[i+5:i+5+length]                
-        if not non_printable:
-                        value = ''.join(c for c in value if c in nvprint) # Remove nonprintables
-        if '<' in value or '>' in value or '&' in value: # Sanatize HTML w/CDATA
-            value = '<![CDATA[' + value.replace(']]>',']]><![CDATA[') + ']]>' 
-        return value,5+length # ** TODO: Verify length by matching end tag **
-    print "Unknown binary format",repr(field[i])
-    return None,-1
+def parse_field(data, offset, field_type, field_len, non_printable):
+    if (len(data) - offset) < field_len:
+        sys.stderr.write("Something went terribly wrong, not enough data for field parsing. Was parsing type %d of length %d but only got %d data\n"
+                             % (field_type, field_len, len(data) - offset))
+        return None
 
-def burp_to_xml(filename,non_printable):
+    if field_type == 0: # INTEGER
+        return str(struct.unpack('>I', data[offset:offset+field_len])[0])
+
+    elif field_type == 1: # LONG
+        unpacked = struct.unpack('>Q', data[offset:offset+field_len])[0]
+        if data[offset] == '\x00': # (64bit) 8 Byte Java Date
+            date = milliseconds_to_date(unpacked)
+            return date if type(date) == str else date.ctime()
+        else: # Serial Number only used ocasionally in Burp
+            return str(unpacked)
+    
+    elif field_type == 2: # BOOLEAN
+        return str(struct.unpack('?', data[offset:offset+field_len])[0])
+    
+    elif field_type == 3: # STRING
+        value = data[offset:offset+field_len]
+        if not non_printable:
+            value = ''.join(c for c in value if c in nvprint) # Remove nonprintables
+        if '<' in value or '>' in value or '&' in value: # Sanatize HTML w/CDATA
+            value = '<![CDATA[' + value.replace(']]>',']]><![CDATA[') + ']]>'
+        return value
+
+def identify_field(data, offset):
+    if (len(data) - offset) < 1:
+        sys.stderr.write("Not enough data, but haven't reached end of stream yet - corrupted data?\n")
+        return None, -1, 0
+    if data[offset] == '\x00': # 4 byte integer value
+        return 0, 4, 1
+    elif data[offset] == '\x01': # unsigned long long type
+        return 1, 8, 1
+    elif data[offset] == '\x02': # Boolean Object True/False
+        return 2, 1, 1
+    elif data[offset] == '\x03' or data[offset] == '\x04': # 4 byte length + string
+        if (len(data) - offset) < 5:
+            return None, -1, 0
+        length = struct.unpack('>I', data[offset+1:offset+5])[0]
+        return 3, length, 5
+    else:
+        sys.stderr.write("Unknown datatype, burp upgraded session files structure?\n")
+        return None, -1, 0
+
+def burp_to_xml(filename, output, non_printable):
     '''Unzip Burp's file, remove non-printable characters, CDATA any HTML,
     include a valid XML header and trailer, and return a valid XML string.
-        if non_printable is true, retain nonprintable characters
-        '''
-
-    xml = '' # Our output string
-    z = zipfile.ZipFile(filename) # Open Burp's zip file
-    burp = z.read('burp','rb') # Read-in the main burp file        
-    m = TAG.match(burp,0) # Match a tag at the start of the string
-    while m:        
-        xml += m.group()            
-        index = m.end()    
-        etag = m.group().replace('<','</') # Matching tag
-        
-        m = TAG.match(burp,index) # Attempt to get the next tag
-        if not m: # Data folows
-            # Read the type of data using Burp's binary data headers        
-            value, length = burp_binary_field(burp, index, non_printable)
-            if value is None: break
-            
-            xml += value
-            xml += etag
-            index += length + len(etag) # Point our index to the next tag
-            m = TAG.match(burp,index) # And retrieve it
+    if non_printable is true, retain nonprintable characters'''
     
-    xml = '<?xml version="1.0"?><burp>' + xml + '</burp>' # XMLify our string
-    return xml # And return it
+    with zipfile.ZipFile(filename) as z:
+        with z.open('burp', 'r') as f:
+            output.write('<?xml version="1.0"?><burp>')
+            chunk = f.read(100) # read just the beginning
+            m = TAG.match(chunk, 0)
+            while m:
+                output.write(m.group())
+                offset = m.end()
+                etag = m.group().replace('<','</')
+                m = TAG.match(chunk, offset)
+                if not m:
+                    if len(chunk) - offset == 0:
+                        break # end of file
+                    field_type, field_len, used_bytes = identify_field(chunk, offset)
+                    offset += used_bytes
+                    if field_type is None:
+                        break # corruption - bad data?
+
+                    remaining = (len(chunk) - offset) - field_len - len(etag);
+                    if remaining < 0:
+                        chunk += f.read(-remaining) # read what we're missing
+                        remaining = 0
+
+                    output.write(parse_field(chunk, offset, field_type, field_len, non_printable))
+                    output.write(etag)
+                    offset += field_len + len(etag)
+
+                    if remaining == 0:
+                        chunk = f.read(CHUNK_SIZE) # force python to free data by re-using the same buffer
+                        offset = 0
+                    elif remaining < 100:
+                        chunk += f.read(CHUNK_SIZE)
+
+                    m = TAG.match(chunk, offset)
+                    
+            output.write('</burp>')
 
 def main():
     '''Called if script is run from the command line.'''
 
-        from argparse import ArgumentParser
-        import sys
+    from argparse import ArgumentParser
+    import sys
 
-        parser = ArgumentParser(description=__doc__)
-        parser.add_argument("-f", "--file", help="Input file (burp session file)")
-        parser.add_argument("-o", "--output", help="Output file (clean XML file)")
-        parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
-        parser.add_argument("-n", "--non-printable", action="store_true", help="Retain non-printable characters")
-        
-        args = parser.parse_args()
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("-f", "--file", help="Input file (burp session file)")
+    parser.add_argument("-o", "--output", help="Output file (clean XML file)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
+    parser.add_argument("-n", "--non-printable", action="store_true", help="Retain non-printable characters")
+    
+    args = parser.parse_args()
 
-        if args.file:
-                if args.output == "-":
-                    out = sys.stdout
-                else:
-                    out = open(args.output, 'wb') if args.output else open(args.file + '.xml', 'wb')
-        else:
-            parser.error('Input file is a mandatory parameter!')
-            print __doc__
-            print parser.print_help()
-            exit(1)
+    if args.file:
+            if args.output == "-":
+                out = sys.stdout
+            else:
+                out = open(args.output, 'wb') if args.output else open(args.file + '.xml', 'wb')
+    else:
+        print __doc__
+        parser.error('Input file is a mandatory parameter!')
 
-    out.write(burp_to_xml(args.file, args.non_printable))
+    burp_to_xml(args.file, out, args.non_printable)
     out.close()
+    
     if args.verbose:
                 sys.stderr.write("# Output written to %s\n" % outfile)
 
